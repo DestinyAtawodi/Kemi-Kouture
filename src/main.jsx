bdfb663eb0d1824724220f72a0ee5232ecbdd9d8
Destiny <atawodidestiny@gmail.com>
2026-09-16 11:14:54 +0100

    router basename removed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/Kemi-Kouture/">
+    <BrowserRouter>
         <App />
     </BrowserRouter>
 )
